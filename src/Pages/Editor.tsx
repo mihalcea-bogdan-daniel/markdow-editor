2d65a937377c3863ad8d0593806ecf97d5fff171
arhitextura <mihalcea.bogdan.daniel@gmail.com>
2022-04-11 18:08:47 +0300

    ~ 40 minute push

--- src/Pages/Editor.tsx
+++ src/Pages/Editor.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import "./Editor.scss";
-import Input from "../components/Input/Input";
+import defaultText from "./defaultText"
+import MarkdownIt from "markdown-it";
+import DOMPurify from "dompurify";
+
 const Editor = () => {
+  const [inputValue, setInputValue] = React.useState(defaultText);
+  const handleInput = (e:React.ChangeEvent<HTMLTextAreaElement>)=>{
+    console.log(e.target.value);
+    
+    
+  }
+  React.useEffect(()=>{
+    setInputValue(defaultText);
+    const md = new MarkdownIt();
+    const safeHtml = DOMPurify.sanitize(md.render(defaultText))
+    console.log(safeHtml);
+  },[])
+
+  React.useEffect(()=>{
+    setInputValue(defaultText);
+    
+    
+  },[inputValue])
+
   return (
     <div className="editor-container">
       <article>
-      <textarea className="text-input" autoFocus ></textarea>
+      <textarea className="text-input" autoFocus onInput={handleInput} defaultValue={inputValue as string}></textarea>
       </article>
+      {/* Needs sanitizier for dangerous HTML elements display will use dompurify for the sake of exercise */}
       <div className="vertical-separator"></div>
-      <aside><textarea className="text-output" disabled ></textarea></aside>
+      <aside></aside>
     </div>
   );
 };
---
+++ src/Pages/defaultText.ts
@@ -0,0 +1,46 @@
+const defaultText:string = `# Simple markdown editor named \`Mark⬇️\`
+
+Built with React for practice based on the [Challenge #3](https://www.frontendmentor.io/challenges/inbrowser-markdown-editor-r16TrrQX9) from [frontendmentor.io](https://www.frontendmentor.io).
+
+## Netlify page 
+[Live example](https://frabjous-kangaroo-bb12c7.netlify.app/)
+
+## Brief
+
+Your challenge is to build out this in-browser markdown editor and get it looking as close to the [design](#design-to-replicate) as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+We provide the data in a local \`data.json file\`, so use that to populate the content on the first load. If you want to take it up a notch, feel free to build this as a full-stack CRUD application!
+
+Your users should be able to:
+* Create, Read, Update, and Delete markdown documents
+* Name and save documents to be accessed as needed
+* Edit the markdown of a document and see the formatted preview of the content
+* View a full-page preview of the formatted content
+* View the optimal layout for the app depending on their device's screen size
+* See hover states for all interactive elements on the page
+* **Bonus**: If you're building a purely front-end project, use localStorage to save the current state in the browser that persists when the browser is refreshed
+* **Bonus**: Build this project as a full-stack application
+
+## Ideas  
+
+* Extend the project to add your own functionality e.g. add support for images, optimise for saving the formatted markdown as a PDF
+* Train your eye for detail by getting your solution as close to the design as you can
+* ~~Use a JavaScript framework/library to practice building using components~~
+* Try estimating the time it will take for you to build the project. Then see if the time taken matches up to your estimate. Project estimations are a skill that is often overlooked but is important for professional developers
+
+## Design to replicate
+
+### Desktop [⬆️](#brief)
+<img src="/notes/Desktop.jpg" alt="dektop" width="400">
+
+### Tablet [⬆️](#brief)
+<img src="/notes/Tablet.jpg" alt="tablet" width="400">
+
+### Mobile [⬆️](#brief)
+<img src="/notes/Mobile.jpg" alt="mobile" width="400">
+
+`
+
+export default defaultText;
