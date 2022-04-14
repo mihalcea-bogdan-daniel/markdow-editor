79b13beeb64ba17e9df5b1b18316e4a5eb22f3e4
arhitextura <mihalcea.bogdan.daniel@gmail.com>
2022-04-14 16:54:01 +0300

    cleanup on layout - tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
 import React from "react";
-import Editor from "./Pages/Editor";
-import { NavBar, NavItem } from "./components/Navigation/Navigation";
-import { DocumentIcon, SaveIcon } from "@heroicons/react/outline";
-import Input from "./components/Input/Input";
-import Button from "./components/Button/Button";
+import MenuIcon from "@mui/icons-material/Menu";
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar logoTitle="MARKDOWN">
-          <NavItem
-            icon={<DocumentIcon width="30px" />}
-            description="Document name"
-          >
-            <Input placeHolder="Document title"></Input>
-          </NavItem>
-          <NavItem>
-            <Button icon={<SaveIcon width="20px" />} buttonText="Save Changes" aria-label="Save changes"></Button>
-          </NavItem>
-        </NavBar>
+    <div className="">
+      <header className="top-0 left-0 absolute w-full bg-zinc-500	">
+        {/* This would be the button */}
+        <nav className="w-full">
+          <div className=" flex h-12 w-12 justify-center items-center bg-zinc-400">
+            {<MenuIcon className="text-zinc-100" />}
+          </div>
+        </nav>
       </header>
-      <main>
-        <Editor></Editor>
+      <main className=" top-12 bottom-12 absolute w-full grid grid-cols-2">
+        <div className="Editor bg-zinc-600">
+          <h2>Markdown</h2>
+          <textarea className="w-full h-full box-border resize-none bg-zinc-800 text-slate-50 focus:outline-none focus:bg-zinc-700" autoFocus></textarea>
+        </div>
       </main>
+      <footer className="bottom-0 left-0 w-full absolute">Some footer</footer>
     </div>
   );
 }
